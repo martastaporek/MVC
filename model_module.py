1086100b7a9a863c78e9b0b9adc2e5bd708c4164
Marta <31178951+morgause@users.noreply.github.com>
2017-10-25 17:37:17 +0200

    implement class ItemList

--- model_module.py
+++ model_module.py
@@ -13,4 +13,21 @@ class Item:
 
     def modify_description(self, description):
         self.description = description
-    
+
+class ItemList:
+
+    def __init__(self):
+        self.item_list = []
+
+    def add_item(self, name, description):
+
+        item = Item(name, description)
+        self.item_list.append(item)
+
+    def delete_item(self, number):
+        number = int(number)
+        number -= 1 #bo indeksy od 0
+        del self.item_list[number]
+
+    def __str__(self):
+        return self.item_list
